bfde5167fb2bcdd45d2e3ed2f97ca2be4aac6831
Chris Xia <rainbowbetter@gmail.com>
2021-03-21 20:04:30 +0800

    commit homework

---
+++ Week01/week01/src/test/java/com/rainbowbetter/week01/XlassLoaderTests.java
@@ -0,0 +1,13 @@
+package com.rainbowbetter.week01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlassLoaderTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
