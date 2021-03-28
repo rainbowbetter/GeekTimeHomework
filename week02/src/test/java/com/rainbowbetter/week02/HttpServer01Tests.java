f1b2017b9f198f7bceb63c9be8db168596a08bf6
Chris Xia <rainbowbetter@gmail.com>
2021-03-28 19:41:29 +0800

    week02 homework

---
+++ week02/src/test/java/com/rainbowbetter/week02/HttpServer01Tests.java
@@ -0,0 +1,13 @@
+package com.rainbowbetter.week02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpServer01Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
